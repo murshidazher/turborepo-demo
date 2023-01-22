4361e78cf09ec36acf81f3e9df968cfe97fe0f4b
murshidazher <hello@murshidazher.com>
2023-01-22 19:20:53 +0530

    feat: add tailwind shared config for nextjs

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return <button className="relative flex items-center justify-center w-full px-8 py-3 text-base font-bold leading-none text-blue-600 transition ease-out border border-transparent rounded-md shadow-lg hover:shadow-2xl bg-gradient-to-b from-white to-blue-50 duration-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 sm:py-4 sm:text-base sm:px-8">Boop</button>;
 };
